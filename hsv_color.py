91fdb6d79c692fac16140861b3eec0fd217daaf5
Brayden <baubs270@gmail.com>
2025-05-07 12:57:01 -0400

    Update hsv_color.py

--- hsv_color.py
+++ hsv_color.py
@@ -1,9 +1,12 @@
+#this code uses opencv to create an hsv color slider to display the different values for each hsv color
+
 import cv2 as cv
 import numpy as np
 
-def nothing(x):
+def nothing(x): 
     pass
 
+#creates window and trackbar
 cv.namedWindow("HSV Frame")
 cv.createTrackbar("H", "HSV Frame", 0, 179, nothing)
 cv.createTrackbar("S", "HSV Frame", 0, 255, nothing)
@@ -11,6 +14,8 @@ cv.createTrackbar("V", "HSV Frame", 0, 255, nothing)
 
 img_hsv = np.zeros((250, 500 ,3), np.uint8)
 
+
+#gets trackbar position
 while True:
     h = cv.getTrackbarPos("H", "HSV Frame")
     s = cv.getTrackbarPos("S", "HSV Frame")
@@ -20,13 +25,8 @@ while True:
     img_rgb = cv.cvtColor(img_hsv, cv.COLOR_HSV2RGB)
 
     cv.imshow("HSV Frame", img_rgb)
-    key = cv.waitKey(1)
+    key = cv.waitKey(1) #waitkey to keep open and running
     if key == 27:
         break
 
 cv.destroyAllWindows()
-
-
-
-
-
